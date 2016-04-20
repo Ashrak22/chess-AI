9be5d406280eace460d29cb63ca33bcef11143ec
Ashrak22 <jakub.simon@gmail.com>
2016-04-20 23:17:35 +0200

    - chessPiece moved from C++ to C#

---
+++ chess-lib/chess-lib/Extensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_lib
+{
+    static class Extensions
+    {
+        public static string MyToString(this COLOUR colour)
+        {
+            switch (colour)
+            {
+                case COLOUR.WHITE:
+                    return "WHITE";
+                case COLOUR.BLACK:
+                    return "BLACK";
+                default:
+                    return "";
+            }
+        }
+
+        public static string MyToString(this STONES type)
+        {
+            switch (type)
+            {
+                case STONES.BISHOP:
+                    return "B";
+                case STONES.EMPTY:
+                    return " ";
+                case STONES.KING:
+                    return "G";
+                case STONES.KNIGHT:
+                    return "K";
+                case STONES.PAWN:
+                    return "P";
+                case STONES.QUEEN:
+                    return "Q";
+                case STONES.ROOK:
+                    return "R";
+                default:
+                    return "";
+            }
+        }
+    }
+}
---
+++ chess-lib/chess-lib/chessPiece.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_lib
+{
+    public enum STONES
+    {
+        EMPTY = 0,
+        PAWN = 1,
+		KNIGHT = 2,
+		BISHOP = 3,
+		ROOK = 4,
+		QUEEN = 5,
+		KING = 6
+    }
+
+    public enum COLOUR
+    {
+        NONE = 0,
+        BLACK = 1,
+		WHITE = 2
+    }
+
+    class chessPiece
+    {
+        public COLOUR colour { get; private set; }
+        public STONES type { get; private set; }
+
+        chessPiece(chessPiece orig = null)
+        {
+            if(orig == null)
+            {
+                colour = orig.colour;
+                type = orig.type;
+            }
+            else
+            {
+                colour = COLOUR.NONE;
+                type = STONES.EMPTY;
+            }
+        }
+        chessPiece(COLOUR orig, STONES stone)
+        {
+            colour = orig;
+            type = stone;
+        }
+
+        void set(chessPiece orig)
+        {
+            set(orig.colour, orig.type);
+        }
+        void set(COLOUR orig, STONES stone)
+        {
+            colour = orig;
+            type = stone;
+        }        
+        void clear()
+        {
+            type = STONES.EMPTY;
+            colour = COLOUR.NONE;
+        }
+
+        public override string ToString()
+        {
+            return type.MyToString();
+        }
+    }
+}
